4395f66a76f1a8260e6999e1d3fc657101ddba8c
slim993 <romdhani.slim.tn@gmail.com>
2022-03-17 16:52:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-jtpcye
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jtpcye)
---
+++ src/app/card-subtitle-example.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Card with sub-title
+ */
+@Component({
+  selector: 'card-subtitle-example',
+  templateUrl: 'card-subtitle-example.html',
+  styleUrls: ['card-subtitle-example.css'],
+})
+export class CardSubtitleExample {
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
